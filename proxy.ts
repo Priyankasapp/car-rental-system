9345f3adedbf71e1c3aceb62d1175c90cb9f6705
Priyankasapp <priyanka.s@applocum.org>
2026-08-12 10:50:41 +0530

    initial commit..

--- proxy.ts
+++ proxy.ts
@@ -206,10 +206,6 @@ export async function proxy(request: NextRequest) {
     return NextResponse.next();
   }
 
- 
-  //  API ROUTES (/api/*)
-
-
   //  Always public 
   if (matchesRoute(path, alwaysPublicRoutes)) {
     return NextResponse.next();
